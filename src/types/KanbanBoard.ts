96519d1f6c112d44fc9fe028e1b0f8d3ff0d4e3b
Yuttakhan B <yuttakhanbaingen@gmail.com>
2022-01-22 18:49:14 +0700

    Move type file

---
+++ src/types/KanbanBoard.ts
@@ -0,0 +1,15 @@
+export type Column = {
+  id: string;
+  name: string;
+  order: number;
+};
+
+export type Card = {
+  id: string;
+  name: string;
+  description: string;
+  createdAt: Date;
+  status: "open" | "closed";
+  order: number;
+  columnId: string;
+};
